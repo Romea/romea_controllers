53ce81c08bbadf95ef71b6fc923183795d1efc23
Vincent Rousseau <vincent.rousseau@irstea.fr>
2016-07-25 16:04:06 +0200

    Remove timer pub cmd in callback

--- ackermann_controller/scripts/teleop_ackermann_joy.py
+++ ackermann_controller/scripts/teleop_ackermann_joy.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import Joy
 
 class TeleopAckermannJoy():
     def __init__(self):
+        self.enable_button = 4
+        self.is_enable_button = False
         self.axis_dead_zone = 0.05
         self.axis_linear = 1
         self.scale_linear = 1
@@ -14,28 +16,25 @@ class TeleopAckermannJoy():
 
         rospy.Subscriber("joy", Joy, self.callback)
         self.pub = rospy.Publisher('cmd_ackermann', AckermannDrive, queue_size=10)
-        rate = rospy.Rate(10.0)
-        self.linear_speed = 0.0
-        self.front_steering = 0.0
 
-        while rospy.is_shutdown() is False:
-            ackermann_msg = AckermannDrive()
-            ackermann_msg.speed = self.linear_speed
-            ackermann_msg.steering_angle = self.front_steering
-            self.pub.publish(ackermann_msg)
-            rate.sleep()
+        rospy.spin()
 
     def callback(self, data):
-        #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.buttons)
-        if abs(data.axes[self.axis_linear]) > self.axis_dead_zone:
-            self.linear_speed = data.axes[self.axis_linear]*self.scale_linear        #left up-down stick
-        else:
-            self.linear_speed = 0.0
+        #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.buttons[self.enable_button])
+        if data.buttons[self.enable_button] is 1:
+            ackermann_msg = AckermannDrive()
 
-        if abs(data.axes[self.axis_front_steering]) > self.axis_dead_zone:
-            self.front_steering = data.axes[self.axis_front_steering]*self.scale_front_steering      #right left-right stick
-        else:
-            self.front_steering = 0.0
+            if abs(data.axes[self.axis_linear]) > self.axis_dead_zone:
+                ackermann_msg.speed = data.axes[self.axis_linear]*self.scale_linear        #left up-down stick
+            else:
+                ackermann_msg.speed = 0.0
+
+            if abs(data.axes[self.axis_front_steering]) > self.axis_dead_zone:
+                ackermann_msg.steering_angle = data.axes[self.axis_front_steering]*self.scale_front_steering      #right left-right stick
+            else:
+                ackermann_msg.steering_angle = 0.0
+
+            self.pub.publish(ackermann_msg)
 
 
 if __name__ == '__main__':
